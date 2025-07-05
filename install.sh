2b82f850963488359df33e647c61c33a7dde0942
W-Kuah <45501552+W-Kuah@users.noreply.github.com>
2025-07-06 06:02:08 +1000

    rename install sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ export JAVA_PROGRAM_ARGS=`echo "$@"`
 mvn clean install
 dependency:copy-dependencies
 mvn package
-mvn exec:java -Dexec.args="$JAVA_PROGRAM_ARGS"
+#mvn exec:java -Dexec.args="$JAVA_PROGRAM_ARGS"
