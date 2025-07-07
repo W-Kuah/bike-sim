ef2e34dc4b771fd9b02021334eddf2610a29f0f7
W-Kuah <45501552+W-Kuah@users.noreply.github.com>
2025-07-07 20:02:59 +1000

    bikesimapp docs

--- src/main/java/org/bikesim/BikeSimApp.java
+++ src/main/java/org/bikesim/BikeSimApp.java
@@ -6,8 +6,16 @@ import org.bikesim.simulator.Simulator;
 
 import java.io.*;
 
-// Detects and handles input source and feeds input lines into CommandProcesor.
+// Detects and handles input source and feeds input lines into CommandParser which could be fed into simulator.
+
+/**
+ * The app that detects and handles input source and feeds input lines into CommandParser object which could be fed into simulator if not null.
+ */
 public class BikeSimApp {
+    /**
+     * <p>Runs when instantiated</p>
+     * @param args the actual string input by the user separate by whitespace into string arrays
+     */
     public static void main(String[] args) {
         Simulator simulator = new Simulator();
         CommandParser parser = new CommandParser();
@@ -26,6 +34,12 @@ public class BikeSimApp {
         }
     }
 
+    /**
+     * <p>Method determines what mode to run in and to handle file errors</p>
+     * @param args string array input from users
+     * @return a FileReader object with a valid input file or input stream reader depending on params
+     * @throws IOException if argument is present and file is not found (or file cannot be read)
+     */
     private static BufferedReader createReader(String[] args) throws IOException {
         if (args.length > 0) {
             // File input mode
